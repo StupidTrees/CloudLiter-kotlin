92964c9a5f96e605bc358bd99f2e0e74f3d7fed9
StupidTrees <2321182768@qq.com>
2020-10-04 19:49:24 +0800

    完善上线下线提醒

--- app/src/main/java/com/stupidtree/hichat/ui/chat/ChatActivity.java
+++ app/src/main/java/com/stupidtree/hichat/ui/chat/ChatActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
